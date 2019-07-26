92509fcce5dafdec0444b7be5a21ba9e35fb1ec6
Peter Wrenn <18408058+pwrenn@users.noreply.github.com>
2019-07-25 18:18:40 -0700

    Completed backend API and scaffolded React app

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
-const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../models/user');
 
 // @route   POST api/users
@@ -20,10 +22,6 @@ router.post(
 		).isLength({ min: 6 })
 	],
 	async (req, res) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).json({ errors: errors.array() });
-		}
 		const { name, email, password } = req.body;
 		try {
 			let user = await User.findOne({ email });
@@ -34,7 +32,22 @@ router.post(
 			const salt = await bcrypt.genSalt(10);
 			user.password = await bcrypt.hash(password, salt);
 			await user.save();
-			res.send('User saved');
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{
+					expiresIn: 360000
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
 		} catch (err) {
 			console.error(error.message);
 			res.status(500).send('Server Error');
