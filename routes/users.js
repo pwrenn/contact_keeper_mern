fbfda2df6e83f552ac13769b80f15677dae3d43f
Peter Wrenn <18408058+pwrenn@users.noreply.github.com>
2019-07-30 22:30:24 -0700

    Finished product before tying the back and front end together

--- routes/users.js
+++ routes/users.js
@@ -22,7 +22,13 @@ router.post(
 		).isLength({ min: 6 })
 	],
 	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
 		const { name, email, password } = req.body;
+
 		try {
 			let user = await User.findOne({ email });
 			if (user) {
